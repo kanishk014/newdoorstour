db520f560a0007a2649f9b7cc2197271b6a9d890
kanishk <kanishk.9871@gmail.com>
2021-04-01 14:21:24 +0530

    Changes

--- templatedata.php
+++ templatedata.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost','root','','doorstour');
+$conn = new mysqli('localhost','jtmdrjci_admin','Jaisairam11*','jtmdrjci_doorstour','3306');
 if($conn->connect_error){
   echo "$conn->connect_error";
   die("Connection Failed : ". $conn->connect_error);
